90e6f1ee1f43be4fd966d81ed761e7855b08710b
Duncan Betts <betts.duncan@tuta.io>
2019-07-30 11:54:37 +0100

    Added another demo test

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,5 +1,11 @@
-describe ('First Test', () => {
-  it ('is working', () => {
-    expect (true).to.equal (true);
+describe("First Test", () => {
+  it("is working", () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe("Visit app - local", () => {
+  it("visits my app", () => {
+    cy.visit("http://localhost:3000");
   });
 });
